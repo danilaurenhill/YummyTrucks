03cbf5372997ff8668f179d6b6efb68eef03c9a2
danilaurenhill <dani.lauren.hill@gmail.com>
2015-04-06 17:22:02 -0400

    finds addresses of food trucks, sometimes...and error notice

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -1,9 +1,10 @@
 class FoodTrucksController < ApplicationController
   def search
     handle = params[:twitter_handle]
-    tweet = TwitterHandleSearch.get_location_tweet(handle)
-    binding.pry
-    # @content = info[:content]
-    # @url = info[:url]
+    @tweet = TwitterHandleSearch.get_location_tweet(handle)
+    if @tweet.nil?
+      @tweet = TwitterHandleSearch.get_newest_tweet(handle)
+      flash[:notice] = "Sorry, we couldn't find a location for that food truck. Here's the truck's most recent Tweet instead."
+    end
   end
 end
--- app/models/twitter_handle_search.rb
+++ app/models/twitter_handle_search.rb
@@ -1,12 +1,15 @@
 class TwitterHandleSearch
+  
+  DESIRABLE_WORDS = [" ave ", " ave.", "avenue", "st.", " st ", "street", "ln", "lane", " sq ", " & ", "Bway", "square"]
+  REGEXES = [/\dst /, /\dnd/, /\drd/, /\dth /]
+
+  UNDESIRABLE_WORDS = ["street food", "street cart"]
+  # URL = "https://twitter.com"
 
   def self.get_newest_tweet(handle)
     client = get_client
     food_truck_account = client.user(handle)
-    most_recent_tweet = food_truck_account.status
-    tweet_content = most_recent_tweet.text
-    url = "http://twitter.com"+ most_recent_tweet.url.path
-    {:content => tweet_content, :url => url}
+    food_truck_account.status
   end
 
   #find recent tweets 
@@ -16,7 +19,23 @@ class TwitterHandleSearch
     client = get_client
     twenty_tweets = client.user_timeline(handle)
     twenty_tweets.each do |tweet|
-      return tweet if tweet.text.downcase.include?(" st.")
+      has_address = false
+      DESIRABLE_WORDS.each do |keyword|
+        has_address = true if tweet.text.downcase.include?(keyword) 
+      end
+      REGEXES.each do |regex|
+        has_address = true if tweet.text.downcase =~ regex 
+      end
+      if has_address && verify_address?(tweet)
+        return tweet
+      end
+    end
+    return nil
+  end
+
+  def self.verify_address?(tweet)
+    UNDESIRABLE_WORDS.each do |word|
+      return false if tweet.text.downcase.include?(word)
     end
   end
 
